78c99b7b968c9aaf1da7e6f225f7389f2c86f81d
Scott Newman <scott.newman@afs.com>
2023-09-18 14:20:05 -0400

    Added bootstrap to the frontend

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -10,7 +8,15 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String index() {
-		return "index";
+		return "index.html";
+	}
+	@GetMapping("/navigation")
+	public String navigation() {
+		return "navigation.html";
 	}
 
+	@GetMapping("/form")
+	public String form() {
+		return "form.html";
+	}
 }
